448848f9aaa3ca236af877953599d908573b65b6
Maria Luiza <malualper@hotmail.com>
2020-06-12 18:46:32 -0300

    negativo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Leia um número inteiro e exiba a mensagem "Você digitou um número negativo."
+            // caso o número seja menor que zero.
+
+            string negativo;
+            int numero;
+
+
+            Console.WriteLine("Digite um numero");
+            negativo=Console.ReadLine();
+            numero = Int32.Parse(negativo);
+            
+            if (numero<0)
+            {
+                Console.WriteLine("Este numero é negativo!");
+            }
+
+            
+
+    
+
+
+
+
+        }
+    }
+}
